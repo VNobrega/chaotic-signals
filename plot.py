38c4edc6847b69265c599bdbc38fc2f71fcce4cc
Nobrega <gjvitor@gmail.com>
2026-04-21 07:24:26 -0300

    plot labels

--- plot.py
+++ plot.py
@@ -6,19 +6,26 @@ from scipy.stats import norm
 n = 10000
 sigma2 = 1
 lags = 10
-W = WGC.genW(np.random.rand(2), sigma2, n)
+W = WGC.genW([0.443, 0.763], sigma2, n)
 T =  WGC.genT(np.random.rand(2), n)
 
 
 
 
-##plot
+# Tent map
 
 autCorrT = np.zeros([2,lags*2+1])
 autCorrW = np.zeros([2, lags*2+1])
 kAxis = np.arange(-lags, lags+1)
 
 
+plt.rcParams.update({
+    'axes.labelsize': 24,
+    'xtick.labelsize': 24,
+    'ytick.labelsize': 24
+})
+
+'''
 fig, ax = plt.subplots(3, 2, figsize=(10,8))
 
 ax[0,0].stem(T[0,:])
@@ -50,43 +57,52 @@ ax[2,1].set_xlabel("k")
 
 plt.tight_layout()
 
+'''
 
-fig, ax = plt.subplots(3, 2, figsize=(10,8))
+#WGC
 
+fig, ax = plt.subplots(3, 2, figsize=(12, 12))
+
+# --- Time series ---
 ax[0,0].stem(W[0,:])
-ax[0,0].set_title("X1")
 ax[0,0].set_xlabel("n")
-ax[0,0].set_ylabel("x")
+ax[0,0].set_ylabel(r"(a) $w_1(n)$")
 
 ax[0,1].stem(W[1,:])
-ax[0,1].set_title("X2")
 ax[0,1].set_xlabel("n")
-ax[0,1].set_ylabel("x")
+ax[0,1].set_ylabel(r"(b) $w_2(n)$")
 
+# --- Histograms ---
 ax[1,0].hist(W[0,:], density=True, bins='fd')
-ax[1,0].set_title("Histogram X1")
 ax[1,0].set_xlim(-3,3)
+ax[1,0].set_xlabel("Amplitude $x_1$")
+ax[1,0].set_ylabel(r"(c) Densidade de $x_1$")
 
 ax[1,1].hist(W[1,:], density=True, bins='fd')
-ax[1,1].set_title("Histogram X2")
 ax[1,1].set_xlim(-3,3)
+ax[1,1].set_xlabel("Amplitude $x_2$")
+ax[1,1].set_ylabel(r"(d) Densidade $x_2$")
 
 eixo = np.linspace(-3,3,1000)
 
 ax[1,0].plot(eixo, norm.pdf(eixo, loc=0, scale=np.sqrt(sigma2)), 'r-', lw=2)
 ax[1,1].plot(eixo, norm.pdf(eixo, loc=0, scale=np.sqrt(sigma2)), 'r-', lw=2)
 
+# --- Autocorrelation ---
 autCorrW[0,:] = WGC.autocorrelation(W[0,:], lags)
 autCorrW[1,:] = WGC.autocorrelation(W[1,:], lags)
 
 ax[2,0].stem(kAxis, autCorrW[0,:])
-ax[2,0].set_ylabel("Rx1x1")
+ax[2,0].set_ylabel(r"(e) R$w_1$$w_1$(k)")
 ax[2,0].set_xlabel("k")
 
 ax[2,1].stem(kAxis, autCorrW[1,:])
-ax[2,1].set_ylabel("Rx2x2")
+ax[2,1].set_ylabel(r"(f) R$w_2$$w_2$(k)")
 ax[2,1].set_xlabel("k")
 
+plt.tight_layout()
+
+'''
 
 rand_vec = np.random.randn(n)
 
@@ -98,9 +114,9 @@ ax_rand.set_xlim(-3,3)
 eixo = np.linspace(-3,3,1000)
 ax_rand.plot(eixo, norm.pdf(eixo, loc=0, scale=1), 'r-', lw=2)
 
-
 plt.tight_layout()
+'''
 
 
-
-
+plt.subplots_adjust(hspace=1)
+plt.show()
